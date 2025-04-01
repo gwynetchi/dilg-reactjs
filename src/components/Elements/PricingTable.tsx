9be758de548973221dd28711ada9a9db13e579bb
gwynetchi <gwynethkyla.ciriaco@cvsu.edu.ph>
2025-04-01 21:11:11 +0800

    changes

--- src/components/Elements/PricingTable.tsx
+++ src/components/Elements/PricingTable.tsx
@@ -2,11 +2,11 @@ import styled from "styled-components";
 // Components
 import FullButton from "../Buttons/FullButton";
 // Assets
-import RollerIcon from "../../assets/svg/Services/RollerIcon";
-import MonitorIcon from "../../assets/svg/Services/MonitorIcon";
-import BrowserIcon from "../../assets/svg/Services/BrowserIcon";
-import PrinterIcon from "../../assets/svg/Services/PrinterIcon";
-import CheckMark from "../../assets/svg/Checkmark";
+// import RollerIcon from "../../assets/svg/Services/RollerIcon";
+// import MonitorIcon from "../../assets/svg/Services/MonitorIcon";
+// import BrowserIcon from "../../assets/svg/Services/BrowserIcon";
+// import PrinterIcon from "../../assets/svg/Services/PrinterIcon";
+// import CheckMark from "../../assets/svg/Checkmark";
 
 interface PricingTableProps {
   icon: string;
@@ -17,36 +17,37 @@ interface PricingTableProps {
   action: () => void;
 }
 
-export default function PricingTable({ icon, price, title, text, offers, action }: PricingTableProps) {
-  let getIcon;
+export default function PricingTable({  icon, price, title, text, offers, action }: PricingTableProps) {
+  //let getIcon;
 
-  switch (icon) {
-    case "roller":
-      getIcon = <RollerIcon />;
-      break;
-    case "monitor":
-      getIcon = <MonitorIcon />;
-      break;
-    case "browser":
-      getIcon = <BrowserIcon />;
-      break;
-    case "printer":
-      getIcon = <PrinterIcon />;
-      break;
-    default:
-      getIcon = <RollerIcon />;
-      break;
-  }
+  // switch (icon) {
+  //   case "roller":
+  //     getIcon = <RollerIcon />;
+  //     break;
+  //   case "monitor":
+  //     getIcon = <MonitorIcon />;
+  //     break;
+  //   case "browser":
+  //     getIcon = <BrowserIcon />;
+  //     break;
+  //   case "printer":
+  //     getIcon = <PrinterIcon />;
+  //     break;
+  //   default:
+  //     getIcon = <RollerIcon />;
+  //     break;
+  // }
 
   return (
     <Wrapper className="whiteBg radius8 shadow">
       <div className="flexSpaceCenter">
-        {getIcon}
         <p className="font30 extraBold">{price}</p>
+        {/* {getIcon} */}
+        
       </div>
       <div style={{ margin: "30px 0" }}>
         <h4 className="font30 extraBold">{title}</h4>
-        <p className="font13">{text}</p>
+        <JustifiedText className="font13">{text}</JustifiedText>
       </div>
       <div>
         {offers
@@ -54,21 +55,21 @@ export default function PricingTable({ icon, price, title, text, offers, action 
               <div className="flexNullCenter" style={{ margin: "15px 0" }} key={index}>
                 <div style={{ position: "relative", top: "-1px", marginRight: "15px" }}>
                   {item.cheked ? (
-                    <div style={{ minWidth: "20px" }}>
-                      <CheckMark />
+                    <div style={{ minWidth: "20px", fontWeight: "bold", fontSize: "16px" }}>
+                      {index + 1}.
                     </div>
                   ) : (
                     <div style={{ minWidth: "20px" }}></div>
                   )}
                 </div>
-                <p className="font20 extraBold">{item.name}</p>
+                <JustifiedText className="font15">{item.name}</JustifiedText>
               </div>
             ))
           : null}
       </div>
-      <div style={{ maxWidth: "120px", margin: "30px auto 0 auto" }}>
+      {/* <div style={{ maxWidth: "120px", margin: "30px auto 0 auto" }}>
         <FullButton title="" action={action} border={undefined} />
-      </div>
+      </div> */}
     </Wrapper>
   );
 }
@@ -77,5 +78,9 @@ const Wrapper = styled.div`
   width: 100%;
   text-align: left;
   padding: 20px 30px;
-  margin-top: 30px;
+  margin-top: 10px;
 `;
+const JustifiedText = styled.p`
+  text-align: justify;
+`;
+
