4fa01f54eb0ddb8cfc8510c57027f193533a7597
gwynetchi <gwynethkyla.ciriaco@cvsu.edu.ph>
2025-04-03 16:31:34 +0800

    latest orgchart

--- src/components/Elements/BlogBox.tsx
+++ src/components/Elements/BlogBox.tsx
@@ -1,40 +1,60 @@
 import styled from "styled-components";
 
+
 interface BlogBoxProps {
   tag: string;
   title: string;
   text: string;
-  action?: () => void;
   author: string;
+  backgroundImage?: string;
+  onClick: () => void; 
+  
 }
 
-export default function BlogBox({ tag, title, text, action, author }: BlogBoxProps) {
+export default function BlogBox({ tag, title, author, backgroundImage, onClick }: BlogBoxProps) {
+  
   return (
-    <WrapperBtn className="animate pointer" onClick={action ? () => action() : undefined}>
-      <Wrapper className="whiteBg radius8 shadow">
-        <h3 className="font20 extraBold">{title}</h3>
+    <WrapperBtn className="animate pointer" onClick={onClick}>
+      <Wrapper className="whiteBg radius8 shadow" backgroundImage={backgroundImage} >
+        <h3 className="font15 extraBold">{title}</h3>
         {/* <p className="font13" style={{ padding: "30px 0" }}>
           {text}
         </p> */}
-        <p className="font13 extraBold">{author}</p>
+        <p className="font11 extraBold">{author}</p>
         <div className="flex">
-          <p className="tag coralBg radius6 font11 extraBold">{tag}</p>
+          <p className="tag coralBg radius6 font13 extraBold">{tag}</p>
         </div>
       </Wrapper>
     </WrapperBtn>
+
+
   );
 }
 
-const Wrapper = styled.div`
+const Wrapper = styled.div<{ backgroundImage?: string}>`
   width: 100%;
   text-align: left;
   padding: 20px 30px;
   margin-top: 30px;
-  
+  background-size: cover;
+  transition: all 0.3s ease;
+  position: relative;
+
+  // height: 100px; /* Initial height for the wrapper */
   transition: background-image 0.3s ease; /* Smooth transition */
+
+  
+  
   &:hover {
-    background-size: cover; /* Ensures image covers the wrapper */
-    // background-image: url('/public/pd.png');
+    background-image: ${({ backgroundImage }) => `url(${backgroundImage})`}; // Apply background image
+    height: calc(30vw / 2); /* Dynamically change height based on image's aspect ratio */
+
+  }
+
+  &:hover p,
+  &:hover h3 {
+    opacity: 0;
+    transition: opacity 0.9s ease;
   }
 `;
 const WrapperBtn = styled.button`
@@ -42,6 +62,7 @@ const WrapperBtn = styled.button`
   outline: none;
   background-color: transparent;
   &:hover {
-    opacity: 0.5;
+    //opacity: 0.5;
   }
 `;
+
